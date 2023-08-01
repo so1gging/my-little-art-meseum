043cbcbb3866c53cad94627dfb384747632ea652
김솔지 <so1gging@gmail.com>
2023-08-01 20:19:18 +0900

    :lipstick: font style 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,16 @@
+'use client'
+import { Jost } from 'next/font/google'
+import GlobalStyle from '@/libs/styles/global'
+
+const jostFont = Jost({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <body>{children}</body>
+  return (
+    <main className={jostFont.className}>
+      <GlobalStyle />
+      {children}
+    </main>
+  )
 }
